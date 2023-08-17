54b7c0cf069994efff7542d9d5d30170b2086407
Afrokk <afrasiyabkhan379@gmail.com>
2023-08-17 18:54:34 -0400

    add css reset

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Fynd.</h1>
+    <h3>Cross-platform Package Tracker.</h3>
   </div>
 </template>
+
+<style lang="sass">
+h1
+  color: $primary
+</style>
